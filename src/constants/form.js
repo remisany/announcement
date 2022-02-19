c48a36eda0787330c3721f8fa5c9e34a1d34acaf
remisany <remi.d.sany@gamil.com>
2022-02-19 16:21:50 +0100

    WIP

--- src/constants/form.js
+++ src/constants/form.js
@@ -1,6 +1,6 @@
 export const required = value => (!value && 'Obligatoire')
 
 export const noSpecialCharacters = value => {
-    const pattern = new RegExp('^[a-zA-Z0-9éèêëàùç\' -]*$')
+    const pattern = new RegExp('^[a-zA-Z0-9éèêëïàâùç\' -]*$')
     return value && !value.match(pattern) && 'Invalide'
 }
